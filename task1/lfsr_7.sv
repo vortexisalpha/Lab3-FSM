ddec6ed56047253f474194012722ed83078db713
an_thony350 <afob2@hotmail.com>
2025-11-12 13:18:26 +0000

    [update] task1 extension

--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -5,4 +5,16 @@ module lfsr_7 (
     output  logic [6:0] data_out
 );
 
+logic [6:0] sreg;
+
+always_ff @(posedge clk, posedge rst) 
+    if (rst)  
+        sreg <= 7'b1;
+    else if(en) 
+        sreg <= {sreg[6:0], sreg[6] ^ sreg[2]};
+    else 
+        sreg <= sreg;
+
+assign data_out = sreg;
+
 endmodule
