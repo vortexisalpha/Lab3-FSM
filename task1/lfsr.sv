59086bda8b855de8d568b35c12e99899a5dff741
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-12 13:11:58 +0000

    lfsr

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -5,4 +5,16 @@ module lfsr(
     output  logic [3:0] data_out
 );
 
+logic [3:0] sreg;
+
+always_ff @(posedge clk, posedge rst) 
+    if (rst)  
+        sreg <= 4'b1;
+    else if(en) 
+        sreg <= {sreg[2:0], sreg[3] ^ sreg[2]};
+    else 
+        sreg <= sreg;
+
+assign data_out = sreg;
+
 endmodule
