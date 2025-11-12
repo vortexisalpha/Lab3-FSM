a2144d1b75d16d3d98d9cd1fd13e0476abe83b7f
vortexisalpha <joshuaboshua250505@gmail.com>
2025-11-12 13:49:56 +0000

    f1_fsm linked with vbuddy

---
+++ task2/f1_fsm_tb.cpp
@@ -0,0 +1,55 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vf1_fsm.h"
+
+#include "vbuddy.cpp"     // include vbuddy code
+#define MAX_SIM_CYC 1000000
+#define ADDRESS_WIDTH 8
+#define ROM_SZ 256
+
+int main(int argc, char **argv, char **env) {
+  int simcyc;     // simulation clock count
+  int tick;       // each clk cycle has two ticks for two edges
+
+  Verilated::commandArgs(argc, argv);
+  // init top verilog instance
+  Vf1_fsm* top = new Vf1_fsm;
+  // init trace dump
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace (tfp, 99);
+  tfp->open ("f1_fsm.vcd");
+ 
+  // init Vbuddy
+  if (vbdOpen()!=1) return(-1);
+  vbdHeader("L3T2: f1_fsm");
+  //vbdSetMode(1);        // Flag mode set to one-shot
+
+  // initialize simulation inputs
+  top->clk = 1;
+  top->rst = 0;
+  top->en = 1;
+
+  // run simulation for MAX_SIM_CYC clock cycles
+  for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
+    // dump variables into VCD file and toggle clock
+    for (tick=0; tick<2; tick++) {
+      tfp->dump (2*simcyc+tick);
+      top->clk = !top->clk;
+      top->eval ();
+    }
+    
+    // plot lights
+    vbdBar(top->data_out & 0xFF);
+
+    vbdCycle(simcyc);
+
+    // either simulation finished, or 'q' is pressed
+    if ((Verilated::gotFinish()) || (vbdGetkey()=='q')) 
+      exit(0);                // ... exit if finish OR 'q' pressed
+  }
+
+  vbdClose();     // ++++
+  tfp->close(); 
+  exit(0);
+}
